49d70a6428198f1aaaaa4088d009da5204cac25e
Alexandra <sunny.belyavskaya@gmail.com>
2020-03-03 15:29:51 +0300

    Updates

---
+++ precache-manifest.caa8697a453744571a53f41a3615e4db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448c79393921ff89ca7d0582f1e8e657",
+    "url": "/Greeting-Friends/index.html"
+  },
+  {
+    "revision": "5dbe6c81c31ac8a9e3e0",
+    "url": "/Greeting-Friends/static/css/main.655051d1.chunk.css"
+  },
+  {
+    "revision": "991a2f90b7b2334bec72",
+    "url": "/Greeting-Friends/static/js/2.e71bbbbc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Greeting-Friends/static/js/2.e71bbbbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dbe6c81c31ac8a9e3e0",
+    "url": "/Greeting-Friends/static/js/main.fed8cd52.chunk.js"
+  },
+  {
+    "revision": "020146c3cb08c7fca556",
+    "url": "/Greeting-Friends/static/js/runtime-main.9bba66a4.js"
+  }
+]);
